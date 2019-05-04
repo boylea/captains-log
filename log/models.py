0f5a0ae46bec83ea6cae63d12363f59a56405e0b
A Boyle <aboyle@hotmail.com>
2019-05-04 12:52:20 -0700

    admin and logitem models

--- log/models.py
+++ log/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class LogItem(models.Model):
+    text = models.TextField()
+    event_date = models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
