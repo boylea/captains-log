6ad155d014fa93519fba4edac85e0e38332ab3cd
A Boyle <aboyle@hotmail.com>
2019-05-04 17:19:53 -0700

    list log items in view

--- log/views.py
+++ log/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import LogItem
 
 def log_list(request):
-    return render(request, 'log/log_list.html', {})
+    log_items = LogItem.objects.all();
+    return render(request, 'log/log_list.html', {'log_items': log_items})
