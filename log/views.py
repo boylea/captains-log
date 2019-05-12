1b0167815388a301b21dcf4394b0ca905188b8d6
A Boyle <aboyle@hotmail.com>
2019-05-12 16:08:34 -0700

    edit existing items

--- log/views.py
+++ log/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import LogItem
 from .forms import LogItemForm
 from django.shortcuts import redirect
@@ -6,13 +6,19 @@ from django.shortcuts import redirect
 def log_list(request):
     print(request)
     if request.method == "POST":
-        form = LogItemForm(request.POST)
+        print(request.POST)
+        if 'update_item' in request.POST:
+            post = get_object_or_404(LogItem, pk=request.POST['update_item'])
+            form = LogItemForm(request.POST, instance=post)
+        else :
+            form = LogItemForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
             return redirect('log_list')
     else:
         log_items = LogItem.objects.all();
-        form = LogItemForm()
-        return render(request, 'log/log_list.html', {'log_items': log_items, 'form': form})
+        existing_forms = [LogItemForm(instance=li) for li in log_items]
+        new_form = LogItemForm()
+        return render(request, 'log/log_list.html', {'log_items': log_items, 'new_form': new_form, 'existing_forms': existing_forms})
 
