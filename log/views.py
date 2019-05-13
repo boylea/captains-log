d42f7ac1cb95542c93cefd44b18d1b8c85e6a567
A Boyle <aboyle@hotmail.com>
2019-05-12 17:17:28 -0700

    save items to correct date

--- log/views.py
+++ log/views.py
@@ -17,19 +17,20 @@ def log_list(request):
 def log_day(request, year, month, day):
     print(year, month, day)
     if request.method == "POST":
-        handle_post(request)
-    
+        handle_post(request, (year, month, day))
+
     items_for_day = LogItem.objects.filter(event_date__exact=datetime.date(year, month, day))
 
     existing_forms = [LogItemForm(instance=li) for li in items_for_day]
     new_form = LogItemForm()
     return render(request, 'log/log_list.html', {'new_form': new_form, 'existing_forms': existing_forms})
 
-def handle_post(request):
+def handle_post(request, date=None):
     print(request.POST)
     if 'delete_item' in request.POST:
         post = get_object_or_404(LogItem, pk=request.POST['delete_item'])
         post.delete()
+        return
 
     if 'update_item' in request.POST:
         post = get_object_or_404(LogItem, pk=request.POST['update_item'])
@@ -38,4 +39,7 @@ def handle_post(request):
         form = LogItemForm(request.POST)
     if form.is_valid():
         post = form.save(commit=False)
+        if date:
+            year, month, day = date
+            post.event_date = datetime.date(year, month, day)
         post.save()
