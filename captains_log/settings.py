cbb3884c52bfc8196e90175dad76d239b7a8a969
Amy Boyle <aboyle5@hotmail.com>
2019-07-06 14:42:06 -0700

    force app inialization

--- captains_log/settings.py
+++ captains_log/settings.py
@@ -15,6 +15,9 @@ import os
 import dj_database_url
 import dotenv
 
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
