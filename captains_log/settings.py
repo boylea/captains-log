a3dc6f4e3c18af674d237d6a5b8243010727638e
Amy Boyle <aboyle5@hotmail.com>
2019-07-06 14:04:38 -0700

    Setup DB based on env vars
    This way I can run with different DBs, e.g. with sqllite locally

--- captains_log/settings.py
+++ captains_log/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,13 +77,11 @@ WSGI_APPLICATION = 'captains_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
